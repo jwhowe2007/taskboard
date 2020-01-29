d9b3ae46744dae17a53c3e615e8b9354fa023f17
jwhowe2007 <justinwhowe1997@reagan.com>
2020-01-29 15:45:36 -0700

    Change App to TaskBoard module

---
+++ src/TaskBoard.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './TaskBoard.css';
+
+function TaskBoard() {
+  return (
+    <div className="board">
+        <div>Lane 1</div>
+        <div>Lane 2</div>
+        <div>Lane 3</div>
+        <div>Lane 4</div>
+    </div>
+  );
+}
+
+export default TaskBoard;
