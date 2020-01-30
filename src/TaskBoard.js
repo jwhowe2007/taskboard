15ce69e2dab02de912cfd697c79b4112ba73f4af
jwhowe2007 <justinwhowe1997@reagan.com>
2020-01-29 17:05:39 -0700

    Set up task board to show four independent swimlanes with add task buttons in each column

--- src/TaskBoard.js
+++ src/TaskBoard.js
@@ -2,15 +2,46 @@ import React from 'react';
 import logo from './logo.svg';
 import './TaskBoard.css';
 
-function TaskBoard() {
-  return (
-    <div className="TaskBoard">
-        <div className="swimlane backlog">Lane 1</div>
-        <div className="swimlane todo">Lane 2</div>
-        <div className="swimlane in-progress">Lane 3</div>
-        <div className="swimlane completed">Lane 4</div>
-    </div>
-  );
+class TaskBoard extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+      // Task arrays per column
+      backlogTasks: [],
+      todoTasks: [],
+      inProgressTasks: [],
+      completedTasks: []
+    }
+  }
+
+  render() {
+    return (
+      <div className="taskboard">
+          <div className="swimlane backlog">
+            <span className="title">Backlog</span>
+
+            <div className="add-task-button">
+              <button>+ Add Task</button>
+            </div>
+          </div>
+
+          <div className="swimlane todo">
+            <span className="title">Todo</span>
+          </div>
+
+          <div className="swimlane in-progress">
+            <span className="title">In Progress</span>
+          </div>
+
+          <div className="swimlane completed">
+            <span className="title">Completed</span>
+
+          </div>
+      </div>
+    );
+  }
 }
 
 export default TaskBoard;
