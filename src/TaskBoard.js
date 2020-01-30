b8278875c883991d9f532147a435737f1303b84f
jwhowe2007 <justinwhowe1997@reagan.com>
2020-01-29 20:34:00 -0700

    Set up task card layout with positional arrows

--- src/TaskBoard.js
+++ src/TaskBoard.js
@@ -9,21 +9,29 @@ class TaskBoard extends React.Component {
 
     this.addTask = this.addTask.bind(this);
 
+    this.firstColumn = "backlog";
+    this.lastColumn = "completed";
+
     this.state = {
 
       // Task arrays per column
       tasks: {
         backlog: [],
         todo: [],
-        inProgressTasks: [],
-        completedTasks: []
+        inProgress: [],
+        completed: []
       }
     }
   }
 
   addTask(column) {
     var choice = prompt('Enter the title for your new task', 'New Task');
-    var newTaskCard = <TaskCard task={ choice } />;
+
+    var newTaskCard = <TaskCard
+      task={ choice }
+      firstColumn={ column === this.firstColumn }
+      lastColumn={ column === this.lastColumn }
+    />;
 
     this.setState((newState, props) => {
 
@@ -34,37 +42,67 @@ class TaskBoard extends React.Component {
   }
 
   render() {
-    var backlogTasks = this.state.tasks.backlog.map((item) => {
+    const backlogTasks = this.state.tasks.backlog.map((card) => {
 
-      return <div>{ item }</div>
+        return card;
+    });
+
+    const todoTasks = this.state.tasks.todo.map((card) => {
+
+        return card;
+    });
+
+    const inProgressTasks = this.state.tasks.inProgress.map((card) => {
+
+        return card;
+    });
+
+    const completedTasks = this.state.tasks.completed.map((card) => {
+
+        return card;
     });
 
     return (
       <div className="taskboard">
-          <div className="swimlane backlog">
-            <span className="title">Backlog</span>
+        <div className="swimlane backlog">
+          <span className="title">Backlog</span>
 
-            <div className="add-task-button">
-              <button onClick={ this.addTask.bind(this, 'backlog') }>+ Add Task</button>
-            </div>
-
-            <div>
-            {backlogTasks}
-            </div>
+          <div className="add-task-button">
+            <button onClick={ this.addTask.bind(this, 'backlog') }>+ Add Task</button>
           </div>
 
-          <div className="swimlane todo">
-            <span className="title">Todo</span>
+          { backlogTasks }
+        </div>
+
+        <div className="swimlane todo">
+          <span className="title">Todo</span>
+
+          <div className="add-task-button">
+            <button onClick={ this.addTask.bind(this, 'todo') }>+ Add Task</button>
           </div>
 
-          <div className="swimlane in-progress">
-            <span className="title">In Progress</span>
+          { todoTasks }
+        </div>
+
+        <div className="swimlane in-progress">
+          <span className="title">In Progress</span>
+
+          <div className="add-task-button">
+            <button onClick={ this.addTask.bind(this, 'inProgress') }>+ Add Task</button>
           </div>
 
-          <div className="swimlane completed">
-            <span className="title">Completed</span>
+          { inProgressTasks }
+        </div>
 
+        <div className="swimlane completed">
+          <span className="title">Completed</span>
+
+          <div className="add-task-button">
+            <button onClick={ this.addTask.bind(this, 'completed') }>+ Add Task</button>
           </div>
+
+          { completedTasks }
+        </div>
       </div>
     );
   }
--- src/TaskCard.js
+++ src/TaskCard.js
@@ -3,10 +3,15 @@ import React from 'react';
 class TaskCard extends React.Component {
 
   render() {
+    const leftArrow = <span className="left-arrow"> &lt; </span>;
+    const rightArrow = <span className="right-arrow"> &gt; </span>;
 
     return (
       <div className="task-card">
+
+        { !this.props.firstColumn ? leftArrow : null }
         <span>{ this.props.task }</span>
+        { !this.props.lastColumn ? rightArrow : null }
       </div>
     );
   }
